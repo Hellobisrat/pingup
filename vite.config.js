b027f981d4f54dd62accfe13ca160bbe4c86dd46
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-11-28 10:23:23 -0600

    connection page completed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
-
-=======
->>>>>>> 452113e7fae75803fa39b6dfec8b9b4ebfc47a2f
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
